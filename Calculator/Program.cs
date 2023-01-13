685f3484ec5069b4ca33b4e54215744694e3c110
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-13 20:39:12 +0000

    Console.WriteLine value

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 50;
+int b = 30;
+int c = a + b;
 
+Console.WriteLine(c);
+Console.ReadKey();
